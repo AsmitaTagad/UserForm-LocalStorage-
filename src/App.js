38d6f13ac40ee9c5b17196617a073ffed5c800cf
AsmitaShivajiTagad <asmitatagad07@gmail.com>
2023-04-11 09:02:37 +0530

    onMouse events & useState Hook,table data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ 
+    const [logs, setLogs] = useState([]);
+
+    function handleMouseOver() {
+      const time = new Date().toLocaleString();
+      const event = { time, eventName:  "in" };
+      setLogs((prevLogs) => [...prevLogs, event]);
+    }
+  
+    function handleMouseOut() {
+      const time = new Date().toLocaleString();
+      const event = { time, eventName:  "out" };
+      setLogs((prevLogs) => [...prevLogs, event]);
+    }
+  
+    return (
+      <div className="container">
+        <div className="sub-container" >
+          <div className="header">
+        <h1 onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
+          Mouse Moment
+        </h1>
+        <h2>.......</h2>
+       </div>
+        <div>
+          <table className="table">
+            <thead>
+              <tr>
+                <th> Date & Time</th>
+                <th>Event Name</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+              logs.map((log, index) => (
+                <tr key={index}>
+                  <td>{log.time}</td>
+                  <td>{log.eventName}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          </div>
+        </div>
+      </div>
+    );
+ 
 }
 
 export default App;
